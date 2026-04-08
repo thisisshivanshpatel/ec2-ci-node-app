8e07911c8b0a0049065dc76730cff4c127329908
Shivansh Patel <shivanshpatel.1999@gmail.com>
2026-04-08 15:50:10 +0300

    added admin route

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello from node ec2 express server 🚀" });
 });
 
+app.get("/admin", (req, res) => {
+  res.json({ message: "Hello admin S002x 🚀" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
